6a3ae4c1ba9505ac232cd14ee129b88a21ea7e91
jooooonj <lee22424205@naver.com>
2023-09-02 19:37:53 +0900

    feat: Jwt 토큰 도입

--- src/main/java/aswemake/project/global/config/SecurityConfig.java
+++ src/main/java/aswemake/project/global/config/SecurityConfig.java
@@ -1,11 +1,15 @@
 package aswemake.project.global.config;
 
+import aswemake.project.global.jwt.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
@@ -13,6 +17,7 @@ import static org.springframework.security.web.util.matcher.AntPathRequestMatche
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -20,16 +25,22 @@ public class SecurityConfig {
                 .securityMatcher("/api/**") // 아래의 모든 설정은 /api/** 경로에만 적용
                 .authorizeHttpRequests(
                         authorizeHttpRequests -> authorizeHttpRequests
-                                .requestMatchers(antMatcher("/**")).permitAll()
+                                .requestMatchers(antMatcher("/api/member/**")).permitAll() // 로그인은 누구나 가능
+                                .requestMatchers(antMatcher(HttpMethod.GET,  "/api/posts/**")).permitAll()
                                 .anyRequest().authenticated() // 나머지는 인증된 사용자만 가능
                 )
+                .cors().disable() // 타 도메인에서 API 호출 가능
+                .csrf().disable() // CSRF 토큰 끄기
+                .httpBasic().disable() // httpBaic 로그인 방식 끄기
+                .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
                 ) // 세션끄기
-                ;
+                .addFilterBefore(
+                        jwtAuthorizationFilter, // 엑세스 토큰으로 부터 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
-
-
 }
