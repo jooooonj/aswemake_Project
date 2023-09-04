7b16314ecff7e382f0678005c1909fae62f108d1
jooooonj <lee22424205@naver.com>
2023-09-04 18:38:23 +0900

    feat: 상품 등록/수정시 상품 스냅샷 생성 로직 구현

--- src/main/java/aswemake/project/domain/product/service/ProductService.java
+++ src/main/java/aswemake/project/domain/product/service/ProductService.java
@@ -1,20 +1,25 @@
 package aswemake.project.domain.product.service;
 
 import aswemake.project.domain.product.entity.Product;
+import aswemake.project.domain.product.entity.ProductSnapshot;
 import aswemake.project.domain.product.entity.request.CreateProductRequestDto;
 import aswemake.project.domain.product.entity.request.ModifyProductPriceRequestDto;
 import aswemake.project.domain.product.entity.response.ProductResponseDto;
 import aswemake.project.domain.product.exception.ProductNotFoundException;
 import aswemake.project.domain.product.repository.ProductRepository;
+import aswemake.project.domain.product.repository.ProductSnapshotRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class ProductService {
     private final ProductRepository productRepository;
+    private final ProductSnapshotRepository productSnapshotRepository;
 
     public Product findById(Long productId){
         return productRepository.findById(productId).orElseThrow(
@@ -25,6 +30,7 @@ public class ProductService {
     @Transactional
     public ProductResponseDto register(CreateProductRequestDto createProductRequestDto) {
         Product product = productRepository.save(Product.create(createProductRequestDto));
+        saveProductSnapshot(product);
         return ProductResponseDto.of(product);
     }
 
@@ -33,6 +39,7 @@ public class ProductService {
                                      ModifyProductPriceRequestDto modifyProductPriceRequestDto) {
         Product product = findById(productId);
         product.modifyPrice(modifyProductPriceRequestDto.getPrice());
+        saveProductSnapshot(product);
         return ProductResponseDto.of(product);
     }
 
@@ -41,4 +48,16 @@ public class ProductService {
         productRepository.deleteById(productId);
     }
 
+    private void saveProductSnapshot(Product product) {
+        ProductSnapshot snapshot = productSnapshotRepository.save(ProductSnapshot.create(product));
+        product.exchangeSnapshot(snapshot);
+    }
+
+    public Integer getProductPriceAtSpecificTime(Long productId, LocalDateTime timestamp) {
+        Integer productPriceAtSpecificTime = productSnapshotRepository.getProductPriceAtSpecificTime(productId, timestamp);
+        if(productPriceAtSpecificTime == null)
+            throw new ProductNotFoundException(timestamp+" 시점에 존재하는 " + productId+"번 상품은 존재하지 않습니다.");
+
+        return productPriceAtSpecificTime;
+    }
 }
