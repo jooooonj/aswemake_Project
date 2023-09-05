7c55a5edbc88921e3f7e3fe94612da170c9bd768
jooooonj <lee22424205@naver.com>
2023-09-05 22:21:16 +0900

    feat: 주문 생성 api 작성 (DTO 유효성 검사 포함)

---
+++ src/main/java/aswemake/project/domain/order/controller/OrderController.java
@@ -0,0 +1,25 @@
+package aswemake.project.domain.order.controller;
+
+import aswemake.project.domain.order.entity.request.CreateOrderRequestDto;
+import aswemake.project.domain.order.service.OrderService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    @PostMapping
+    public ResponseEntity<Long> create(@AuthenticationPrincipal User user,
+                                       @Valid @RequestBody CreateOrderRequestDto createOrderRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                orderService.createOrder(user.getUsername(), createOrderRequestDto));
+    }
+}
