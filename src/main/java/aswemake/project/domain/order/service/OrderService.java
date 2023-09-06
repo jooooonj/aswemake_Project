b89c0d03fff621eac95c339ea3d9bc49ef22cb8f
jooooonj <lee22424205@naver.com>
2023-09-06 20:43:12 +0900

    feat: 주문에 대한 결제 금액을 조회하는 서비스 로직을 구현한다.

--- src/main/java/aswemake/project/domain/order/service/OrderService.java
+++ src/main/java/aswemake/project/domain/order/service/OrderService.java
@@ -1,5 +1,10 @@
 package aswemake.project.domain.order.service;
 
+import aswemake.project.domain.coupon.entity.DiscountCoupon;
+import aswemake.project.domain.coupon.entity.FixedDiscountCoupon;
+import aswemake.project.domain.coupon.entity.PercentageDiscountCoupon;
+import aswemake.project.domain.coupon.exception.CouponNotFoundException;
+import aswemake.project.domain.coupon.service.DiscountCouponService;
 import aswemake.project.domain.member.entity.Member;
 import aswemake.project.domain.member.service.MemberService;
 import aswemake.project.domain.order.entity.Order;
@@ -13,7 +18,6 @@ import aswemake.project.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +28,19 @@ public class OrderService {
     private final OrderRepository orderRepository;
     private final MemberService memberService;
     private final ProductService productService;
+    private final DiscountCouponService discountCouponService;
+
+    public Order findById(Long orderId) {
+        return orderRepository.findById(orderId).orElseThrow(
+                () -> new OrderNotFoundException("존재하지 않는 주문입니다.")
+        );
+    }
+
+    public OrderItem findOrderItemByOrderItemId(Long orderItemId) {
+        return orderRepository.findOrderItemByOrderItemId(orderItemId).orElseThrow(
+                () -> new OrderNotFoundException("존재하지 않는 주문품목입니다.")
+        );
+    }
 
     @Transactional
     public Long createOrder(String username, CreateOrderRequestDto createOrderRequestDto) {
@@ -35,10 +52,22 @@ public class OrderService {
 
     public Integer getOrderPrice(Long orderId) {
         Integer orderPrice = orderRepository.findOrderPriceByOrderId(orderId);
-        if(orderPrice == null)
+        if (orderPrice == null)
             throw new OrderNotFoundException("해당 주문은 존재하지 않습니다.");
 
         return orderPrice;
+    }
+
+    public Integer calculatePaymentPrice(Long orderId, Long orderItemId, String couponCode) {
+        Order order = findById(orderId);
+        if (couponCode == null)
+            return order.getTotalPrice();
+
+        if (orderItemId == null) //전체 주문에 쿠폰을 적용하는 경우
+            return calculatePaymentWithCouponForAllOrder(couponCode, order);
+
+        //특정 상품에 쿠폰을 적용하는 경우
+        return calculatePaymentWithCouponForSpecificProduct(couponCode, order, orderItemId);
     }
 
     private List<OrderItem> createOrderItems(CreateOrderRequestDto createOrderRequestDto) {
@@ -50,5 +79,35 @@ public class OrderService {
             orderItems.add(orderItem);
         }
         return orderItems;
+    }
+
+    //쿠폰이 전체 주문에 적용된 가격을 계산한다.
+    private Integer calculatePaymentWithCouponForAllOrder(String couponCode, Order order) {
+        DiscountCoupon coupon = discountCouponService.findByCouponCode(couponCode);
+
+        if (coupon instanceof FixedDiscountCoupon)
+            return order.getTotalPrice() - ((FixedDiscountCoupon) coupon).getFixedDiscountAmount();
+
+        if (coupon instanceof PercentageDiscountCoupon)
+            return order.getTotalPrice() - (order.getTotalPriceWithoutDeliveryFee()) * ((PercentageDiscountCoupon) coupon).getDiscountPercentage() / 100;
+
+        throw new CouponNotFoundException("존재하지 않는 쿠폰 유형입니다.");
+    }
+
+    //쿠폰이 특정 상품에 적용된 가격을 계산한다.
+    private Integer calculatePaymentWithCouponForSpecificProduct(String couponCode, Order order, Long orderItemId) {
+        OrderItem orderItem = findOrderItemByOrderItemId(orderItemId);
+        if(orderItem.getOrder().getId() != order.getId())
+            throw new IllegalArgumentException("해당 주문 품목은 " + order.getId() + "번 주문에 속한 주문이 아닙니다.");
+
+        DiscountCoupon coupon = discountCouponService.findByCouponCode(couponCode);
+
+        if (coupon instanceof FixedDiscountCoupon)
+            return order.getTotalPrice() - ((FixedDiscountCoupon) coupon).getFixedDiscountAmount();
+
+        if (coupon instanceof PercentageDiscountCoupon)
+            return order.getTotalPrice() - (orderItem.getOrderItemPrice() * ((PercentageDiscountCoupon) coupon).getDiscountPercentage() / 100);
+
+        throw new CouponNotFoundException("존재하지 않는 쿠폰 유형입니다.");
     }
 }
