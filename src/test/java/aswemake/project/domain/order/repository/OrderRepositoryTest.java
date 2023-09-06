5fc4598094b01886890c4898a53f30be60af8e9a
jooooonj <lee22424205@naver.com>
2023-09-06 20:45:38 +0900

    test: 결제금액 조회 서비스 로직에 대한 테스트 작성

--- src/test/java/aswemake/project/domain/order/repository/OrderRepositoryTest.java
+++ src/test/java/aswemake/project/domain/order/repository/OrderRepositoryTest.java
@@ -3,11 +3,16 @@ package aswemake.project.domain.order.repository;
 import aswemake.project.domain.member.entity.Member;
 import aswemake.project.domain.member.repository.MemberRepository;
 import aswemake.project.domain.order.entity.Order;
+import aswemake.project.domain.order.entity.OrderItem;
+import aswemake.project.domain.product.entity.Product;
+import aswemake.project.domain.product.repository.ProductRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
 
 @DataJpaTest
 class OrderRepositoryTest {
@@ -15,6 +20,9 @@ class OrderRepositoryTest {
     protected OrderRepository orderRepository;
     @Autowired
     protected MemberRepository memberRepository;
+    @Autowired
+    protected ProductRepository productRepository;
+
     @Test
     @DisplayName("findOrderPriceByOrderId - 특정 주문에 대한 총금액을 조회한다.")
     void findOrderPriceByOrderId() {
@@ -33,5 +41,38 @@ class OrderRepositoryTest {
 
         //then
         Assertions.assertThat(orderPriceByOrderId).isEqualTo(order.getTotalPrice());
+    }
+
+    @Test
+    @DisplayName("findOrderItemByOrderItemId - orderItemId 값에 대한 orderItem을 반환한다.")
+    void findOrderItemByOrderItemId() {
+        //given
+        int quantity = 5;
+        Long orderItemId = 1L;
+        Member member = memberRepository.save(Member.builder().email("member@naver.com").password("1234").build());
+        Product product = productRepository.save(Product.builder()
+                .id(1L)
+                .productName("테스트 상품")
+                .price(10000)
+                .build());
+        OrderItem orderItem = OrderItem.builder()
+                .product(product)
+                .quantity(5)
+                .orderItemPrice(product.getPrice() * 5)
+                .build();
+        Order order = Order.builder()
+                .member(member)
+                .deliveryFee(5000)
+                .totalPrice(10000)
+                .build();
+        order.connectOrderItems(List.of(orderItem));
+        Order savedorder = orderRepository.save(order);
+
+        //when
+        OrderItem findOrderItem = orderRepository.findOrderItemByOrderItemId(orderItemId).get();
+
+        //then
+        Assertions.assertThat(findOrderItem.getQuantity()).isEqualTo(quantity);
+        Assertions.assertThat(findOrderItem.getOrderItemPrice()).isEqualTo(product.getPrice() * quantity);
     }
 }
--- src/test/java/aswemake/project/domain/order/service/OrderServiceTest.java
+++ src/test/java/aswemake/project/domain/order/service/OrderServiceTest.java
@@ -1,9 +1,14 @@
 package aswemake.project.domain.order.service;
 
+import aswemake.project.domain.coupon.entity.FixedDiscountCoupon;
+import aswemake.project.domain.coupon.entity.PercentageDiscountCoupon;
+import aswemake.project.domain.coupon.entity.scopeType.CouponApplyScope;
+import aswemake.project.domain.coupon.service.DiscountCouponService;
 import aswemake.project.domain.member.entity.Member;
 import aswemake.project.domain.member.entity.MemberRoleType;
 import aswemake.project.domain.member.service.MemberService;
 import aswemake.project.domain.order.entity.Order;
+import aswemake.project.domain.order.entity.OrderItem;
 import aswemake.project.domain.order.entity.request.CreateOrderRequestDto;
 import aswemake.project.domain.order.entity.request.OrderItemRequestDto;
 import aswemake.project.domain.order.exception.OrderNotFoundException;
@@ -18,9 +23,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
-
+import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
@@ -37,6 +41,8 @@ class OrderServiceTest {
     protected ProductService productService;
     @Mock
     protected MemberService memberService;
+    @Mock
+    protected DiscountCouponService couponService;
 
     @Test
     @DisplayName("주문을 생성한다.")
@@ -97,5 +103,190 @@ class OrderServiceTest {
         });
 
         Assertions.assertThat(exception.getMessage()).isEqualTo("해당 주문은 존재하지 않습니다.");
+    }
+
+    @Test
+    @DisplayName("결제금액을 조회한다. 쿠폰 코드가 null값이면 단순 주문의 총금액을 반환한다.")
+    void calculatePaymentPrice1() {
+        //given
+        Long orderId = 1L;
+        Order order = Order.builder()
+                .totalPrice(50000) // 주문 총금액 5만원
+                .build();
+        given(orderRepository.findById(anyLong())).willReturn(Optional.of(order));
+
+        //when
+        Integer calculatePaymentPrice = orderService.calculatePaymentPrice(orderId, null, null);
+
+        //then
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(order.getTotalPrice());
+    }
+
+    @Test
+    @DisplayName("결제금액을 조회한다. (전체 주문에 대해서 고정할인 쿠폰을 적용한다.)")
+    void calculatePaymentPrice2() {
+        //given
+        Long orderId = 1L;
+        String couponCode = "2만원_고정할인_전체주문";
+        int orderTotalPrice = 50000;
+        int discountAmount = 20000;
+
+        Order order = Order.builder()
+                .totalPrice(orderTotalPrice) // 주문 총금액 5만원
+                .build();
+        FixedDiscountCoupon fixedDiscountCoupon = FixedDiscountCoupon.createStub
+                (discountAmount, CouponApplyScope.ORDER_ALL, couponCode);
+
+        given(orderRepository.findById(anyLong())).willReturn(Optional.of(order));
+        given(couponService.findByCouponCode(anyString())).willReturn(fixedDiscountCoupon);
+
+        //when
+        Integer calculatePaymentPrice = orderService.calculatePaymentPrice(orderId, null, couponCode);
+
+        //then
+        log.info("실제 작동 로직 - {} ", "order.getTotalPrice() - ((FixedDiscountCoupon) coupon).getFixedDiscountAmount()");
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(order.getTotalPrice() - fixedDiscountCoupon.getFixedDiscountAmount());
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(orderTotalPrice - discountAmount);
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(50000 - 20000);
+    }
+
+    @Test
+    @DisplayName("결제금액을 조회한다. (특정 상품에 대해서 고정할인 쿠폰을 적용한다.)")
+    void calculatePaymentPrice3() {
+        //given
+        Long orderId = 1L;
+        String couponCode = "2만원_고정할인_특정상품";
+        Long orderItemId = 1L;
+        int orderTotalPrice = 50000;
+        int discountAmount = 20000;
+
+        Order order = Order.builder()
+                .id(1L)
+                .totalPrice(orderTotalPrice) // 주문 총금액 5만원
+                .build();
+        OrderItem orderItem = OrderItem.builder()
+                .order(order)
+                .orderItemPrice(30000)
+                .build(); //주문품목 금액 3만원
+
+        FixedDiscountCoupon fixedDiscountCoupon = FixedDiscountCoupon.createStub
+                (discountAmount, CouponApplyScope.ORDER_ALL, couponCode);
+
+        given(orderRepository.findById(anyLong())).willReturn(Optional.of(order));
+        given(couponService.findByCouponCode(anyString())).willReturn(fixedDiscountCoupon);
+        given(orderRepository.findOrderItemByOrderItemId(anyLong())).willReturn(Optional.of(orderItem));
+
+        //when
+        Integer calculatePaymentPrice = orderService.calculatePaymentPrice(orderId, orderItemId, couponCode);
+
+        //then
+        log.info("실제 작동 로직 - {} ", "order.getTotalPrice() - ((FixedDiscountCoupon) coupon).getFixedDiscountAmount()");
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(order.getTotalPrice() - fixedDiscountCoupon.getFixedDiscountAmount());
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(orderTotalPrice - discountAmount);
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(50000 - 20000);
+    }
+
+    @Test
+    @DisplayName("결제금액을 조회한다. (주문품목이 주문이 가지고 있는 품목이 아니면 예외발생)")
+    void calculatePaymentPrice_exception() {
+        //given
+        Long orderId = 1L;
+        String couponCode = "2만원_고정할인_특정상품";
+        Long orderItemId = 1L;
+        int orderTotalPrice = 50000;
+        int discountAmount = 20000;
+
+        Order order = Order.builder()
+                .id(1L)
+                .totalPrice(orderTotalPrice) // 주문 총금액 5만원
+                .build();
+        Order order_fail = Order.builder()
+                .id(2L)
+                .totalPrice(orderTotalPrice) // 주문 총금액 5만원
+                .build();
+
+        OrderItem orderItem = OrderItem.builder()
+                .order(order_fail)
+                .orderItemPrice(30000)
+                .build(); //주문품목 금액 3만원
+
+        FixedDiscountCoupon fixedDiscountCoupon = FixedDiscountCoupon.createStub
+                (discountAmount, CouponApplyScope.SPECIFIC_PRODUCT, couponCode);
+
+        given(orderRepository.findById(anyLong())).willReturn(Optional.of(order));
+        given(orderRepository.findOrderItemByOrderItemId(anyLong())).willReturn(Optional.of(orderItem));
+
+        //when, then
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> {
+            orderService.calculatePaymentPrice(orderId, orderItemId, couponCode);
+        });
+
+        Assertions.assertThat(exception.getMessage()).isEqualTo("해당 주문 품목은 " + order.getId() + "번 주문에 속한 주문이 아닙니다.");
+    }
+
+    @Test
+    @DisplayName("결제금액을 조회한다. (전체 주문에 대해서 비율할인 쿠폰을 적용한다.)")
+    void calculatePaymentPrice4() {
+        //given
+        Long orderId = 1L;
+        String couponCode = "10퍼센트_비율할인_전체주문";
+        int orderTotalPrice = 50000;
+        int discountPercent = 10;
+
+        Order order = Order.builder()
+                .id(1L)
+                .deliveryFee(5000)
+                .totalPrice(orderTotalPrice) // 주문 총금액 5만원
+                .build();
+
+        PercentageDiscountCoupon percentageDiscountCoupon = PercentageDiscountCoupon.createStub
+                (discountPercent, CouponApplyScope.ORDER_ALL, couponCode);
+
+        given(orderRepository.findById(anyLong())).willReturn(Optional.of(order));
+        given(couponService.findByCouponCode(anyString())).willReturn(percentageDiscountCoupon);
+
+        //when
+        Integer calculatePaymentPrice = orderService.calculatePaymentPrice(orderId, null, couponCode);
+
+        //then
+        log.info("실제 작동 로직 - {} ", "order.getTotalPrice() - (order.getTotalPriceWithoutDeliveryFee()) * ((PercentageDiscountCoupon) coupon).getDiscountPercentage() / 100");
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(order.getTotalPrice() - (order.getTotalPriceWithoutDeliveryFee()) * percentageDiscountCoupon.getDiscountPercentage() / 100);
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(50000 - (50000 - 5000) * 10 / 100);
+    }
+
+    @Test
+    @DisplayName("결제금액을 조회한다. (특정 상품에 대해서 비율할인 쿠폰을 적용한다.)")
+    void calculatePaymentPrice5() {
+        //given
+        Long orderId = 1L;
+        String couponCode = "10퍼센트_비율할인_전체주문";
+        Long orderItemId = 1L;
+        int orderTotalPrice = 50000;
+        int discountPercent = 10;
+
+        Order order = Order.builder()
+                .id(1L)
+                .deliveryFee(5000)
+                .totalPrice(orderTotalPrice) // 주문 총금액 5만원
+                .build();
+        OrderItem orderItem = OrderItem.builder()
+                .order(order)
+                .orderItemPrice(30000)
+                .build();
+
+        PercentageDiscountCoupon percentageDiscountCoupon = PercentageDiscountCoupon.createStub
+                (discountPercent, CouponApplyScope.SPECIFIC_PRODUCT, couponCode);
+
+        given(orderRepository.findById(anyLong())).willReturn(Optional.of(order));
+        given(couponService.findByCouponCode(anyString())).willReturn(percentageDiscountCoupon);
+        given(orderRepository.findOrderItemByOrderItemId(anyLong())).willReturn(Optional.of(orderItem));
+
+        //when
+        Integer calculatePaymentPrice = orderService.calculatePaymentPrice(orderId, orderItemId, couponCode);
+
+        //then
+        log.info("실제 작동 로직 - {} ", "order.getTotalPrice() - (orderItem.getOrderItemPrice() * ((PercentageDiscountCoupon) coupon).getDiscountPercentage() / 100)");
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(order.getTotalPrice() - (orderItem.getOrderItemPrice() * percentageDiscountCoupon.getDiscountPercentage() / 100));
+        Assertions.assertThat(calculatePaymentPrice).isEqualTo(50000 - (30000 * 10 / 100));
     }
 }
